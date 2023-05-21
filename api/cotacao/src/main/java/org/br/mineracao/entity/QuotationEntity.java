27d07438790595ec0076ccd657dc285155304da2
Jairo Nascimento <jaironsousa@gmail.com>
2023-05-21 13:02:16 -0300

    Inserido a biblioteca lombok Criação da classe QuotationEntity

---
+++ api/cotacao/src/main/java/org/br/mineracao/entity/QuotationEntity.java
@@ -0,0 +1,34 @@
+package org.br.mineracao.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * @Autor Jairo Nascimento
+ * @Created 21/05/2023 - 12:56
+ */
+
+@Entity
+@Table(name = "quotation")
+@Data
+@NoArgsConstructor
+public class QuotationEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private Date date;
+
+    @Column(name = "currency_price")
+    private BigDecimal currencyPrice;
+
+    @Column(name = "pct_change")
+    private String pctChange;
+
+    private String pair;
+}
